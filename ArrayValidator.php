0047513fafd4d053d78582066eb131436c7d70d2
Роман Сохарев <greabock@gmail.com>
2014-08-21 03:15:49 +0700

    Устал комитить - пошел сериал залипать...

--- ArrayValidator.php
+++ ArrayValidator.php
@@ -7,12 +7,12 @@ class ArrayValidator{
     
     public function __call($name, $args)
     {
-        return call_user_func_array([self ,$name . '_'], $args);
+        return call_user_func_array([ self, $name . '_' ], $args);
     }
     
     public static function __callStatic($name, $args)
     {
-        return call_user_func_array([self,$name . '_'], $args);
+        return call_user_func_array([ self, $name . '_' ], $args);
     }
     
     private function arraySameKeys_(Array $array1, Array $array2)
